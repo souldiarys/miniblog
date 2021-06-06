72dc698eef33af866e2101eab3f8e05fb7bb27ed
souldiarys <souldiarys@naver.com>
2021-06-06 13:29:02 +0900

    [CH1] 미니블로그 - URL/Template 추가

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,30 @@
+from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
-# Create your views here.
+# Home
+def home(request):
+    return render(request, 'blog/home.html')
+
+# About
+def about(request):
+    return render(request, 'blog/about.html')
+
+# Contact
+def contact(request):
+    return render(request, 'blog/contact.html')
+
+# Dashboard
+def dashboard(request):
+    return render(request, 'blog/dashboard.html')
+
+# Logout
+def user_logout(request):
+    return HttpResponseRedirect('/')
+
+# Signup
+def signup(request):
+    return render(request, 'blog/signup.html')
+
+# Login
+def user_login(request):
+    return render(request, 'blog/login.html')
--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
 ]
